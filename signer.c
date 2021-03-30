eecc1453bfa4348c532c7d596af3a09c315a6375
Sternbauer <k118112499@students.jku.at>
2021-03-30 15:24:28 +0200

    restructure order of methods in signer class

--- signer.c
+++ signer.c
@@ -80,6 +80,7 @@ static const char *i_value = 0;
 
 
 /* ---------- Function header definition ---------- */
+/* Main command functionality */
 static int keygen(const char *keyname, const char *parm_set);
 static int sign(const char *keyname, char **files);
 static int sign_bulk(const char *keyname, char **files);
@@ -87,21 +88,26 @@ static int verify(const char *keyname, char **files);
 static int verify_bulk (const char *keyname, char **files);
 static int advance(const char *keyname, const char *text_advance);
 
-static void usage(char *program);
+/* For key generation */
+bool do_rand( void *output, size_t len );
+static bool convert_specified_seed_i_value( void *, size_t );
+static int fromhex(char c);
 static void list_parameter_set(int levels, const param_set_t *lm_array,
                            const param_set_t *ots_array, size_t aux_size );
 static int parse_parm_set( int *levels, param_set_t *lm_array,
                            param_set_t *ots_array, size_t *aux_size,
                            const char *parm_set);
+static int get_integer(const char **p);
+
+/* For signing and verification */
 static bool read_private_key( unsigned char *private_key,
                               size_t len_private_key, void *filename);                           
 static bool update_private_key( unsigned char *private_key,
                                size_t len_private_key, void *filename);
-bool do_rand( void *output, size_t len );
-static bool convert_specified_seed_i_value( void *, size_t );
-static int get_integer(const char **p);
-static int fromhex(char c);
 void *read_file( const char *filename, size_t *len );
+
+/* For main */
+static void usage(char *program);
 
 /* ---------- Method implementations ---------- */
 
@@ -588,154 +594,7 @@ static int advance(const char *keyname, const char *text_advance) {
 
 /* ---------- NON COMMAND METHODS - Auxiliary functions */
 
-/*
- * This retrieves the private key from secure storage; in this case, a file on
- * the filesystem.  The context pointer we use here is the filename
- */
-static bool read_private_key( unsigned char *private_key,
-                              size_t len_private_key, void *filename) {
-    FILE *f = fopen( filename, "r" );
-    if (!f) {
-        return false;
-    }
-    if (1 != fread( private_key, len_private_key, 1, f )) {
-        /* Read failed */
-        fclose(f);
-        return false;
-    }
-    fclose(f);
-
-    /* Everything succeeded */
-    return true;
-}
-
-/*
- * This parses the parameter set; this is provided so we can try different
- * sets without recompiling the program each time.  This is placed here
- * because it's ugly parsing code that has nothing to do with how to use
- * HSS
- */
-static int parse_parm_set( int *levels, param_set_t *lm_array,
-                           param_set_t *ots_array, size_t *aux_size,
-                           const char *parm_set) {
-    int i;
-    size_t aux = DEFAULT_AUX_DATA;
-    for (i=0;; i++) {
-        if (i == 8) {
-            printf( "Error: more than 8 HSS levels specified\n" );
-            return 0;
-        }
-        /* Get the number of levels of this tree */
-        int h = get_integer( &parm_set );
-        param_set_t lm;
-        switch (h) {
-        case 5:  lm = LMS_SHA256_N32_H5;  break;
-        case 10: lm = LMS_SHA256_N32_H10; break;
-        case 15: lm = LMS_SHA256_N32_H15; break;
-        case 20: lm = LMS_SHA256_N32_H20; break;
-        case 25: lm = LMS_SHA256_N32_H25; break;
-        case 0: printf( "Error: expected height of Merkle tree\n" ); return 0;
-        default: printf( "Error: unsupported Merkle tree height %d\n", h );
-                 printf( "Supported heights = 5, 10, 15, 20, 25\n" );
-                 return 0;
-        }
-        /* Now see if we can get the Winternitz parameter */
-        param_set_t ots = LMOTS_SHA256_N32_W8;
-        if (*parm_set == '/') {
-            parm_set++;
-            int w = get_integer( &parm_set );
-            switch (w) {
-            case 1: ots = LMOTS_SHA256_N32_W1; break;
-            case 2: ots = LMOTS_SHA256_N32_W2; break;
-            case 4: ots = LMOTS_SHA256_N32_W4; break;
-            case 8: ots = LMOTS_SHA256_N32_W8; break;
-            case 0: printf( "Error: expected Winternitz parameter\n" ); return 0;
-            default: printf( "Error: unsupported Winternitz parameter %d\n", w );
-                     printf( "Supported parmaeters = 1, 2, 4, 8\n" );
-                     return 0;
-            }
-        }
-
-        lm_array[i] = lm;
-        ots_array[i] = ots;
-
-        if (*parm_set == ':') {
-            parm_set++;
-            aux = get_integer( &parm_set );
-            break;
-        }
-        if (*parm_set == '\0') break;
-        if (*parm_set == ',') { parm_set++; continue; }
-        printf( "Error: parse error after tree specification\n" ); return 0;
-    }
-
-    *levels = i+1;
-    *aux_size = aux;
-    return 1;
-}
-
-/**
- * Lists the parameters which will be used for generating the key pair 
- */
-static void list_parameter_set(int levels, const param_set_t *lm_array,
-                           const param_set_t *ots_array, size_t aux_size ) {
-    printf( "Parameter set being used: there are %d levels of Merkle trees\n", levels );
-    int i;
-    for (i=0; i<levels; i++) {
-        printf( "Level %d: hash function = SHA-256; ", i );
-        int h = 0;
-        switch (lm_array[i]) {
-        case LMS_SHA256_N32_H5:  h = 5; break;
-        case LMS_SHA256_N32_H10: h = 10; break;
-        case LMS_SHA256_N32_H15: h = 15; break;
-        case LMS_SHA256_N32_H20: h = 20; break;
-        case LMS_SHA256_N32_H25: h = 25; break;
-        }
-        printf( "%d level Merkle tree; ", h );
-        int w = 0;
-        switch (ots_array[i]) {
-        case LMOTS_SHA256_N32_W1: w = 1; break;
-        case LMOTS_SHA256_N32_W2: w = 2; break;
-        case LMOTS_SHA256_N32_W4: w = 4; break;
-        case LMOTS_SHA256_N32_W8: w = 8; break;
-        }
-        printf( "Winternitz param %d\n", w );
-    }
-    if (aux_size > 0) {
-        printf( "Maximum of %lu bytes of aux data\n", (unsigned long)aux_size );
-    } else {
-        printf( "Aux data disabled\n" );
-    }
-}
-
-/*
- * This saves the private key to secure storage; in this case, a file on the
- * filesystem.  The context pointer we use here is the filename
- */
-static bool update_private_key( unsigned char *private_key,
-                               size_t len_private_key, void *filename) {
-    FILE *f = fopen( filename, "r+" );
-    if (!f) {
-        /* Open failed, possibly because the file didn't exist */
-        f = fopen( filename, "w" );
-        if (!f) {
-            /* Unable to open file */
-            return false;
-        }
-    }
-    if (1 != fwrite( private_key, len_private_key, 1, f )) {
-        /* Write failed */
-        fclose(f);
-        return false;
-    }
-    if (0 != fclose(f)) {
-        /* Close failed (possibly because pending write failed) */
-        return false;
-    }
-
-    /* Everything succeeded */
-    return true;
-}
+/* Methods for generating the key pair */
 
 /*
  * This is a function that is supposed to generate truly random values.
@@ -808,33 +667,6 @@ bool do_rand( void *output, size_t len ) {
     return true;
 }
 
-static int get_integer(const char **p) {
-    int n = 0;
-
-    while (isdigit(**p)) {
-        n = 10*n + **p - '0';
-        *p += 1;
-    }
-
-    return n;
-}
-
-/**
- * Parses a DEC integer value (0 .. 15) from a hex number
- */ 
-static int fromhex(char c) {
-    if (isdigit(c)) return c - '0';
-    switch (c) {
-    case 'a': case 'A': return 10;
-    case 'b': case 'B': return 11;
-    case 'c': case 'C': return 12;
-    case 'd': case 'D': return 13;
-    case 'e': case 'E': return 14;
-    case 'f': case 'F': return 15;
-    default: return 0;  /* Turn any nonhexdigit into a 0 */
-    }
-}
-
 /*
  * This is used if the user maually specified the seed and the
  * i values
@@ -855,6 +687,184 @@ static bool convert_specified_seed_i_value( void *buffer, size_t len) {
         *out++ = 16*c + d;
     }
 
+    return true;
+}
+
+/**
+ * Parses a DEC integer value (0 .. 15) from a hex number
+ */ 
+static int fromhex(char c) {
+    if (isdigit(c)) return c - '0';
+    switch (c) {
+    case 'a': case 'A': return 10;
+    case 'b': case 'B': return 11;
+    case 'c': case 'C': return 12;
+    case 'd': case 'D': return 13;
+    case 'e': case 'E': return 14;
+    case 'f': case 'F': return 15;
+    default: return 0;  /* Turn any nonhexdigit into a 0 */
+    }
+}
+
+/**
+ * Lists the parameters which will be used for generating the key pair 
+ */
+static void list_parameter_set(int levels, const param_set_t *lm_array,
+                           const param_set_t *ots_array, size_t aux_size ) {
+    printf( "Parameter set being used: there are %d levels of Merkle trees\n", levels );
+    int i;
+    for (i=0; i<levels; i++) {
+        printf( "Level %d: hash function = SHA-256; ", i );
+        int h = 0;
+        switch (lm_array[i]) {
+        case LMS_SHA256_N32_H5:  h = 5; break;
+        case LMS_SHA256_N32_H10: h = 10; break;
+        case LMS_SHA256_N32_H15: h = 15; break;
+        case LMS_SHA256_N32_H20: h = 20; break;
+        case LMS_SHA256_N32_H25: h = 25; break;
+        }
+        printf( "%d level Merkle tree; ", h );
+        int w = 0;
+        switch (ots_array[i]) {
+        case LMOTS_SHA256_N32_W1: w = 1; break;
+        case LMOTS_SHA256_N32_W2: w = 2; break;
+        case LMOTS_SHA256_N32_W4: w = 4; break;
+        case LMOTS_SHA256_N32_W8: w = 8; break;
+        }
+        printf( "Winternitz param %d\n", w );
+    }
+    if (aux_size > 0) {
+        printf( "Maximum of %lu bytes of aux data\n", (unsigned long)aux_size );
+    } else {
+        printf( "Aux data disabled\n" );
+    }
+}
+
+/*
+ * This parses the parameter set; this is provided so we can try different
+ * sets without recompiling the program each time.  This is placed here
+ * because it's ugly parsing code that has nothing to do with how to use
+ * HSS
+ */
+static int parse_parm_set( int *levels, param_set_t *lm_array,
+                           param_set_t *ots_array, size_t *aux_size,
+                           const char *parm_set) {
+    int i;
+    size_t aux = DEFAULT_AUX_DATA;
+    for (i=0;; i++) {
+        if (i == 8) {
+            printf( "Error: more than 8 HSS levels specified\n" );
+            return 0;
+        }
+        /* Get the number of levels of this tree */
+        int h = get_integer( &parm_set );
+        param_set_t lm;
+        switch (h) {
+        case 5:  lm = LMS_SHA256_N32_H5;  break;
+        case 10: lm = LMS_SHA256_N32_H10; break;
+        case 15: lm = LMS_SHA256_N32_H15; break;
+        case 20: lm = LMS_SHA256_N32_H20; break;
+        case 25: lm = LMS_SHA256_N32_H25; break;
+        case 0: printf( "Error: expected height of Merkle tree\n" ); return 0;
+        default: printf( "Error: unsupported Merkle tree height %d\n", h );
+                 printf( "Supported heights = 5, 10, 15, 20, 25\n" );
+                 return 0;
+        }
+        /* Now see if we can get the Winternitz parameter */
+        param_set_t ots = LMOTS_SHA256_N32_W8;
+        if (*parm_set == '/') {
+            parm_set++;
+            int w = get_integer( &parm_set );
+            switch (w) {
+            case 1: ots = LMOTS_SHA256_N32_W1; break;
+            case 2: ots = LMOTS_SHA256_N32_W2; break;
+            case 4: ots = LMOTS_SHA256_N32_W4; break;
+            case 8: ots = LMOTS_SHA256_N32_W8; break;
+            case 0: printf( "Error: expected Winternitz parameter\n" ); return 0;
+            default: printf( "Error: unsupported Winternitz parameter %d\n", w );
+                     printf( "Supported parmaeters = 1, 2, 4, 8\n" );
+                     return 0;
+            }
+        }
+
+        lm_array[i] = lm;
+        ots_array[i] = ots;
+
+        if (*parm_set == ':') {
+            parm_set++;
+            aux = get_integer( &parm_set );
+            break;
+        }
+        if (*parm_set == '\0') break;
+        if (*parm_set == ',') { parm_set++; continue; }
+        printf( "Error: parse error after tree specification\n" ); return 0;
+    }
+
+    *levels = i+1;
+    *aux_size = aux;
+    return 1;
+}
+
+static int get_integer(const char **p) {
+    int n = 0;
+
+    while (isdigit(**p)) {
+        n = 10*n + **p - '0';
+        *p += 1;
+    }
+
+    return n;
+}
+
+/* For signing and verification */
+
+/*
+ * This retrieves the private key from secure storage; in this case, a file on
+ * the filesystem.  The context pointer we use here is the filename
+ */
+static bool read_private_key( unsigned char *private_key,
+                              size_t len_private_key, void *filename) {
+    FILE *f = fopen( filename, "r" );
+    if (!f) {
+        return false;
+    }
+    if (1 != fread( private_key, len_private_key, 1, f )) {
+        /* Read failed */
+        fclose(f);
+        return false;
+    }
+    fclose(f);
+
+    /* Everything succeeded */
+    return true;
+}
+
+/*
+ * This saves the private key to secure storage; in this case, a file on the
+ * filesystem.  The context pointer we use here is the filename
+ */
+static bool update_private_key( unsigned char *private_key,
+                               size_t len_private_key, void *filename) {
+    FILE *f = fopen( filename, "r+" );
+    if (!f) {
+        /* Open failed, possibly because the file didn't exist */
+        f = fopen( filename, "w" );
+        if (!f) {
+            /* Unable to open file */
+            return false;
+        }
+    }
+    if (1 != fwrite( private_key, len_private_key, 1, f )) {
+        /* Write failed */
+        fclose(f);
+        return false;
+    }
+    if (0 != fclose(f)) {
+        /* Close failed (possibly because pending write failed) */
+        return false;
+    }
+
+    /* Everything succeeded */
     return true;
 }
 
@@ -896,6 +906,8 @@ void *read_file( const char *filename, size_t *len ) {
     if (len) *len = cur_len;
     return p;
 }
+
+/* For main */
 
 /**
  * Lists how to use the commands of this program.
